d7d813066cc7ff8006e92445a947bae3f3058e41
Benjamin-Kyalo <benjahshariz@gmail.com>
2024-01-05 09:20:12 +0300

    first commit

---
+++ counterapp/code.js
@@ -0,0 +1,57 @@
+// document.getElementById("count-el").innerText = 5;
+
+// let myAge = 23
+// console.log(myAge)
+
+// let firstbatch = 5;
+// let secondbatch = 7;
+// let count = firstbatch + secondbatch;
+// console.log(count);
+
+// let myAge = 23
+// let humandogratio = 7
+// let mydogage = myAge * humandogratio;
+// console.log(mydogage);
+
+// let bonuspoints = 50
+// bonuspoints = bonuspoints + 50;
+// console.log(bonuspoints);
+// bonuspoints = bonuspoints - 75;
+// console.log(bonuspoints);
+// bonuspoints = bonuspoints + 45;
+// console.log(bonuspoints);
+// let count = 0
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+// function countdown() {
+//   console.log(5);
+//   console.log(4);
+//   console.log(3);
+//   console.log(2);
+//   console.log(1);
+// }
+// countdown();
+
+// function number() {
+//     console.log(42)
+// }
+// number()
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+// function totallaptimes () {
+//     console.log(lap1 + lap2 + lap3)
+// }
+// totallaptimes()
+
+let lapscompleted = 0
+function incrementby1() {
+    lapscompleted = lapscompleted + 1
+}
+incrementby1()
+incrementby1()
+incrementby1()
+console.log(lapscompleted)
+
