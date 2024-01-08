5fd8c91c1809331f730f576763c9c555f89a469f
Benjamin-Kyalo <benjahshariz@gmail.com>
2024-01-08 13:08:20 +0300

    additions

---
+++ jspractices/trial.js
@@ -0,0 +1,31 @@
+// let firstname = "Benjamin ";
+// let secondname = "Kyalo";
+// let fullname = firstname + secondname;
+// console.log(fullname);
+
+// let name = "Linda";
+// let greeting = "Hi there";
+// let output = greeting + ", " + name + "!";
+// console.log(output);
+// function greetLinda() {
+//   console.log(greeting + ", " + name + "!");
+// }
+
+// greetLinda();
+
+// let mypoints = 3;
+// function add3points() {
+//   mypoints += 3;
+// }
+
+// function remove1point() {
+//   mypoints -= 1;
+// }
+
+// add3points();
+// add3points();
+// add3points();
+// remove1point();
+// remove1point();
+// console.log(mypoints);
+
