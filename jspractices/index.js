3592a7466576aa728f5da1dcdfdcb360a0d8581e
Benjamin-Kyalo <benjahshariz@gmail.com>
2024-01-10 10:52:00 +0300

    calculator
    calculator

--- jspractices/index.js
+++ jspractices/index.js
@@ -1,3 +1,7 @@
-function purchase(){
-    
+let errorParagraph = document.getElementById("error");
+console.log(errorParagraph);
+
+function purchase() {
+  console.log("button clicked");
+  errorParagraph.textContent = "Something went wrong, please try again";
 }
